f93f780534cab6d64020e18097dc2d7c3a6d8131
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-03-18 01:38:56 -0400

    Created Direction Node
    Created the Direction node to enable associating a 3D directional vector with an integer value. Used to associate each face of a die with a value. getDirectionUnit() method calculates the current direction based on the node's global transform combined with the Endpoint vector.

---
+++ nodes/src/direction.cc
@@ -0,0 +1,26 @@
+#include <direction.hh>
+#include <godot_cpp/core/property_info.hpp>
+
+using namespace godot;
+
+void Direction::_bind_methods()
+{
+	ClassDB::bind_method(D_METHOD("getEndpoint"), &Direction::getEndpoint);
+	ClassDB::bind_method(D_METHOD("setEndpoint", "p_point"), &Direction::setEndpoint);
+	ClassDB::add_property("Direction", PropertyInfo(Variant::VECTOR3, "Endpoint"), "setEndpoint", "getEndpoint");
+	
+	ClassDB::bind_method(D_METHOD("getValue"), &Direction::getValue);
+	ClassDB::bind_method(D_METHOD("setValue", "p_value"), &Direction::setValue);
+	ClassDB::add_property("Direction", PropertyInfo(Variant::INT, "Value"), "setValue", "getValue");
+	
+	ClassDB::bind_method(D_METHOD("getDirectionUnit"), &Direction::getDirectionUnit);
+}
+
+Vector3 Direction::getDirectionUnit() const
+{
+	return get_global_transform()
+		.basis
+		.xform(Endpoint)
+		.normalized()
+		.round();
+}
---
+++ nodes/src/direction.hh
@@ -0,0 +1,33 @@
+#ifndef DIRECTION_HH
+#define DIRECTION_HH
+
+#include <godot_cpp/classes/node3d.hpp>
+
+namespace godot
+{
+	class Direction : public Node3D
+	{
+		GDCLASS(Direction, Node3D)
+		
+		public:
+			Vector3 getDirectionUnit() const;
+			
+			Vector3 getEndpoint() const { return Endpoint; }
+			void setEndpoint(const Vector3 point) { Endpoint = point; }
+			
+			int getValue() const { return Value; }
+			void setValue(const int value) { Value = value; }
+		
+		protected:
+			static void _bind_methods();
+		
+		private:
+			Vector3 Endpoint;
+			int Value;
+			
+			Direction() = default;
+			~Direction() = default;
+	};
+}
+
+#endif //DIRECTION_HH
