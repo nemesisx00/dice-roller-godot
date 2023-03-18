e4264dc4518391802029a88c71417fe0cd7b51c1
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-03-18 19:56:57 -0400

    Updated DieNode
    Removed PrintOnSleep node.
    Updated DieNode to extend RigidBody3D and emit a new signal, "asleep," passing its instance id when it falls asleep; Also, added a Sides property for convenience.
    Had to regenerate the nodes.gdextension file for some reason.
    Updated the dice scenes to use the new DieNode as their root node and set a Sides value for each.

--- nodes/src/dienode.cc
+++ nodes/src/dienode.cc
@@ -1,5 +1,6 @@
 #include <dienode.hh>
 
+#include <godot_cpp/classes/engine.hpp>
 #include <direction.hh>
 
 using namespace godot;
@@ -7,16 +8,29 @@ using namespace godot;
 void DieNode::_bind_methods()
 {
 	ClassDB::bind_method(D_METHOD("getValue"), &DieNode::getValue);
+	ClassDB::bind_method(D_METHOD("handleSleepingStateChange"), &DieNode::handleSleepingStateChange);
+	
+	ClassDB::bind_method(D_METHOD("getSides"), &DieNode::getSides);
+	ClassDB::bind_method(D_METHOD("setSides", "p_sides"), &DieNode::setSides);
+	ClassDB::add_property("DieNode", PropertyInfo(Variant::INT, "Sides"), "setSides", "getSides");
+	
+	ADD_SIGNAL(MethodInfo("asleep", PropertyInfo(Variant::INT, "p_id")));
 }
 
-void DieNode::_ready() {}
+void DieNode::_ready()
+{
+	isEditor = Engine::get_singleton()->is_editor_hint();
+	
+	if(!isEditor)
+		connect("sleeping_state_changed", Callable(this, "handleSleepingStateChange"));
+}
 
 int DieNode::getValue() const
 {
 	int value = -1;
 	
 	Vector3 up(0, 1, 0);
-	TypedArray<Node> children = get_children();
+	TypedArray<Node> children = get_node<Node3D>("%Sides")->get_children();
 	int childrenSize = children.size();
 	for(int i = 0; i < childrenSize; i++)
 	{
@@ -29,4 +43,10 @@ int DieNode::getValue() const
 	}
 	
 	return value;
+}
+
+void DieNode::handleSleepingStateChange()
+{
+	if(is_sleeping())
+		emit_signal("asleep", get_instance_id());
 }
--- nodes/src/dienode.hh
+++ nodes/src/dienode.hh
@@ -1,22 +1,43 @@
 #ifndef DIENODE_HH
 #define DIENODE_HH
 
-#include <godot_cpp/classes/node3d.hpp>
+#include "dicemanager.hh"
+#include <godot_cpp/classes/rigid_body3d.hpp>
 
 namespace godot
 {
-	class DieNode : public Node3D
+	enum Dice
 	{
-		GDCLASS(DieNode, Node3D)
+		Four = 4,
+		Six = 6, 
+		Eight = 8,
+		Ten = 10,
+		Twelve = 12,
+		Twenty = 20,
+		Hundred = 100,
+	};
+	
+	class DieNode : public RigidBody3D
+	{
+		GDCLASS(DieNode, RigidBody3D)
 		
 		public:
 			void _ready() override;
-			int getValue() const;
 			
+			int getValue() const;
+			void handleSleepingStateChange();
+			
+			int getSides() const { return Sides; };
+			void setSides(const int sides) { Sides = sides; }
+		
 		protected:
 			static void _bind_methods();
 		
 		private:
+			bool isEditor;
+			
+			int Sides;
+			
 			DieNode() = default;
 			~DieNode() = default;
 	};
