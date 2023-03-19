5a3a1109700aac59976f50860de0896daa3e5915
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-03-18 23:06:45 -0400

    Update dicemanager.cc
    Added a randomized impulse force to each die when it is spawned.

--- nodes/src/dicemanager.cc
+++ nodes/src/dicemanager.cc
@@ -84,6 +84,7 @@ void DiceManager::handleSpawnDice(const int sides, const int quantity = 1)
 			std::mt19937 gen(rd());
 			std::uniform_real_distribution<> pdist(-5, 5);
 			std::uniform_real_distribution<> rdist(-1.5, 1.5);
+			std::uniform_real_distribution<> fdist(-25, 25);
 			
 			Vector3 x(1, 0, 0);
 			Vector3 y(0, 1, 0);
@@ -91,14 +92,15 @@ void DiceManager::handleSpawnDice(const int sides, const int quantity = 1)
 			
 			for(int i = 0; i < quantity; i++)
 			{
-				Node3D *die = Node::cast_to<Node3D>(resource->instantiate());
-				die->translate(Vector3(pdist(gen), 25, pdist(gen)));
+				DieNode *die = Node::cast_to<DieNode>(resource->instantiate());
+				die->translate(Vector3(pdist(gen), 20, pdist(gen)));
 				die->rotate_object_local(x, rdist(gen));
 				die->rotate_object_local(y, rdist(gen));
 				die->rotate_object_local(z, rdist(gen));
 				die->connect("asleep", Callable(this, "handleAsleep"));
 				nodeIds.push_back(die->get_instance_id());
 				add_child(die);
+				die->apply_impulse(Vector3(fdist(gen), 0, fdist(gen)));
 			}
 		}
 	}
