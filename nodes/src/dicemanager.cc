653575963989749cd1d270d76c456136712fa17f
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-03-19 21:06:26 -0400

    Refined DiceManager
    Added new Properties for each of the random generation limits, as well as SpawnHeight and GroupImpulse. SpawnHeight is self-explanatory but GroupImpulse is a boolean determining whether or not a single impulse vector is shared by each die type or if a new impulse vector is generated for every individual die.
    
    Cleaned up the resource loading code by using a godot::String and formatting in the number of sides.

--- nodes/src/dicemanager.cc
+++ nodes/src/dicemanager.cc
@@ -25,6 +25,50 @@ void DiceManager::_bind_methods()
 	ClassDB::bind_method(D_METHOD("handleSpawnDice"), &DiceManager::handleSpawnDice);
 	
 	ADD_SIGNAL(MethodInfo("roll_completed"));
+	
+	ClassDB::add_property_group("DiceManager", "Impulse at Spawn", "");
+	
+	ClassDB::bind_method(D_METHOD("getGroupImpulse"), &DiceManager::getGroupImpulse);
+	ClassDB::bind_method(D_METHOD("setGroupImpulse", "p_min"), &DiceManager::setGroupImpulse);
+	ClassDB::add_property("DiceManager", PropertyInfo(Variant::BOOL, "Group Impulse by Die Type"), "setGroupImpulse", "getGroupImpulse");
+	
+	ClassDB::bind_method(D_METHOD("getDownforceMax"), &DiceManager::getDownforceMax);
+	ClassDB::bind_method(D_METHOD("setDownforceMax", "p_max"), &DiceManager::setDownforceMax);
+	ClassDB::add_property("DiceManager", PropertyInfo(Variant::FLOAT, "Maximum Downforce"), "setDownforceMax", "getDownforceMax");
+		
+	ClassDB::bind_method(D_METHOD("getDownforceMin"), &DiceManager::getDownforceMin);
+	ClassDB::bind_method(D_METHOD("setDownforceMin", "p_min"), &DiceManager::setDownforceMin);
+	ClassDB::add_property("DiceManager", PropertyInfo(Variant::FLOAT, "Minimum Downforce"), "setDownforceMin", "getDownforceMin");
+	
+	ClassDB::bind_method(D_METHOD("getForceMax"), &DiceManager::getForceMax);
+	ClassDB::bind_method(D_METHOD("setForceMax", "p_max"), &DiceManager::setForceMax);
+	ClassDB::add_property("DiceManager", PropertyInfo(Variant::FLOAT, "Maximum Lateral Force"), "setForceMax", "getForceMax");
+	
+	ClassDB::bind_method(D_METHOD("getForceMin"), &DiceManager::getForceMin);
+	ClassDB::bind_method(D_METHOD("setForceMin", "p_min"), &DiceManager::setForceMin);
+	ClassDB::add_property("DiceManager", PropertyInfo(Variant::FLOAT, "Minimum Lateral Force"), "setForceMin", "getForceMin");
+	
+	ClassDB::add_property_group("DiceManager", "Transform at Spawn", "");
+	
+	ClassDB::bind_method(D_METHOD("getPositionMax"), &DiceManager::getPositionMax);
+	ClassDB::bind_method(D_METHOD("setPositionMax", "p_max"), &DiceManager::setPositionMax);
+	ClassDB::add_property("DiceManager", PropertyInfo(Variant::FLOAT, "Position Maximum"), "setPositionMax", "getPositionMax");
+	
+	ClassDB::bind_method(D_METHOD("getPositionMin"), &DiceManager::getPositionMin);
+	ClassDB::bind_method(D_METHOD("setPositionMin", "p_min"), &DiceManager::setPositionMin);
+	ClassDB::add_property("DiceManager", PropertyInfo(Variant::FLOAT, "Position Minimum"), "setPositionMin", "getPositionMin");
+			
+	ClassDB::bind_method(D_METHOD("getRotationMax"), &DiceManager::getRotationMax);
+	ClassDB::bind_method(D_METHOD("setRotationMax", "p_max"), &DiceManager::setRotationMax);
+	ClassDB::add_property("DiceManager", PropertyInfo(Variant::FLOAT, "Rotation Max"), "setRotationMax", "getRotationMax");
+	
+	ClassDB::bind_method(D_METHOD("getRotationMin"), &DiceManager::getRotationMin);
+	ClassDB::bind_method(D_METHOD("setRotationMin", "p_min"), &DiceManager::setRotationMin);
+	ClassDB::add_property("DiceManager", PropertyInfo(Variant::FLOAT, "Rotation Min"), "setRotationMin", "getRotationMin");
+	
+	ClassDB::bind_method(D_METHOD("getSpawnHeight"), &DiceManager::getSpawnHeight);
+	ClassDB::bind_method(D_METHOD("setSpawnHeight", "p_height"), &DiceManager::setSpawnHeight);
+	ClassDB::add_property("DiceManager", PropertyInfo(Variant::FLOAT, "Spawn Height"), "setSpawnHeight", "getSpawnHeight");
 }
 
 void DiceManager::_ready()
@@ -67,55 +111,68 @@ void DiceManager::handleSpawnDice(const int sides, const int quantity = 1)
 {
 	if(quantity > 0)
 	{
+		String path("res://dice/d{0}.tscn");
+		Array params;
 		Ref<PackedScene> resource;
 		switch(sides)
 		{
 			case DiceType::Four:
-				resource = ResourceLoader::get_singleton()->load("res://dice/d4.tscn");
+				params.push_back(DiceType::Four);
 				break;
 			case DiceType::Six:
-				resource = ResourceLoader::get_singleton()->load("res://dice/d6.tscn");
+				params.push_back(DiceType::Six);
 				break;
 			case DiceType::Eight:
-				resource = ResourceLoader::get_singleton()->load("res://dice/d8.tscn");
+				params.push_back(DiceType::Eight);
 				break;
 			case DiceType::Ten:
-				resource = ResourceLoader::get_singleton()->load("res://dice/d10.tscn");
+				params.push_back(DiceType::Ten);
 				break;
 			case DiceType::Twelve:
-				resource = ResourceLoader::get_singleton()->load("res://dice/d12.tscn");
+				params.push_back(DiceType::Twelve);
 				break;
 			case DiceType::Twenty:
-				resource = ResourceLoader::get_singleton()->load("res://dice/d20.tscn");
+				params.push_back(DiceType::Twenty);
 				break;
 			case DiceType::Hundred:
-				resource = ResourceLoader::get_singleton()->load("res://dice/d100.tscn");
+				params.push_back(DiceType::Hundred);
 				break;
 		}
+		
+		if(params.size() > 0)
+			resource = ResourceLoader::get_singleton()->load(path.format(params));
 		
 		if(resource->can_instantiate())
 		{
 			std::random_device rd;
 			std::mt19937 gen(rd());
-			std::uniform_real_distribution<> pdist(-5, 5);
-			std::uniform_real_distribution<> rdist(-1.5, 1.5);
-			std::uniform_real_distribution<> fdist(-50, 50);
+			std::uniform_real_distribution<> pos(PositionMin, PositionMax);
+			std::uniform_real_distribution<> rot(RotationMin, RotationMax);
+			std::uniform_real_distribution<> force(ForceMin, ForceMax);
+			std::uniform_real_distribution<> dforce(DownforceMin, DownforceMax);
 			
 			Vector3 x(1, 0, 0);
 			Vector3 y(0, 1, 0);
 			Vector3 z(0, 0, 1);
 			
+			Vector3 impulse(force(gen), dforce(gen), force(gen));
+			
 			for(int i = 0; i < quantity; i++)
 			{
 				DieNode *die = Node::cast_to<DieNode>(resource->instantiate());
-				die->translate(Vector3(pdist(gen), 15, pdist(gen)));
-				die->rotate_object_local(x, rdist(gen));
-				die->rotate_object_local(y, rdist(gen));
-				die->rotate_object_local(z, rdist(gen));
+				die->translate(Vector3(pos(gen), SpawnHeight, pos(gen)));
+				die->rotate_object_local(x, rot(gen));
+				die->rotate_object_local(y, rot(gen));
+				die->rotate_object_local(z, rot(gen));
 				die->connect("asleep", Callable(this, "handleAsleep"));
 				nodeIds.push_back(die->get_instance_id());
+				
 				add_child(die);
-				die->apply_impulse(Vector3(fdist(gen), 0, fdist(gen)));
+				
+				if(GroupImpulse)
+					die->apply_impulse(impulse);
+				else
+					die->apply_impulse(Vector3(force(gen), dforce(gen), force(gen)));
 			}
 		}
 	}
--- nodes/src/dicemanager.hh
+++ nodes/src/dicemanager.hh
@@ -16,12 +16,53 @@ namespace godot
 			void handleClearDice();
 			void handleSpawnDice(const int sides, const int quantity);
 			
+			float_t getDownforceMax() const { return DownforceMax; }
+			void setDownforceMax(const float_t max) { DownforceMax = max; }
+			
+			float_t getDownforceMin() const { return DownforceMin; }
+			void setDownforceMin(const float_t min) { DownforceMin = min; }
+			
+			float_t getForceMax() const { return ForceMax; }
+			void setForceMax(const float_t max) { ForceMax = max; }
+			
+			float_t getForceMin() const { return ForceMin; }
+			void setForceMin(const float_t min) { ForceMin = min; }
+			
+			bool getGroupImpulse() const { return GroupImpulse; }
+			void setGroupImpulse(const bool group) { GroupImpulse = group; }
+			
+			float_t getPositionMax() const { return PositionMax; }
+			void setPositionMax(const float_t max) { PositionMax = max; }
+			
+			float_t getPositionMin() const { return PositionMin; }
+			void setPositionMin(const float_t min) { PositionMin = min; }
+			
+			float_t getRotationMax() const { return RotationMax; }
+			void setRotationMax(const float_t max) { RotationMax = max; }
+			
+			float_t getRotationMin() const { return RotationMin; }
+			void setRotationMin(const float_t min) { RotationMin = min; }
+			
+			float_t getSpawnHeight() const { return SpawnHeight; }
+			void setSpawnHeight(const float_t height) { SpawnHeight = height; }
+			
 		protected:
 			static void _bind_methods();
 		
 		private:
 			bool isEditor;
 			std::list<uint64_t> nodeIds;
+			
+			float_t DownforceMax = -1;
+			float_t DownforceMin = -5;
+			float_t ForceMax = 50;
+			float_t ForceMin = -50;
+			bool GroupImpulse = true;
+			float_t PositionMax = 15;
+			float_t PositionMin = -15;
+			float_t RotationMax = 3;
+			float_t RotationMin = -3;
+			float_t SpawnHeight = 15;
 			
 			DiceManager() = default;
 			~DiceManager() = default;
