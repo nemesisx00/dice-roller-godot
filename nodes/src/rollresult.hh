29babdc3ef803a3880580eb437ff449daf73d630
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-03-21 19:58:53 -0400

    Update rollresult.hh
    Moved the member fields to the top of the definition.
    Added a new constructor, taking a godot::Dictionary as its initial value.
    Added method into(), which constructs a godot::Dictionary from the struct's values.

--- nodes/src/rollresult.hh
+++ nodes/src/rollresult.hh
@@ -1,37 +1,89 @@
 #ifndef ROLLRESULT_HH
 #define ROLLRESULT_HH
 
+#include <godot_cpp/variant/variant.hpp>
+#include <godot_cpp/variant/dictionary.hpp>
+#include <godot_cpp/variant/typed_array.hpp>
 #include <algorithm>
 #include <functional>
 #include <random>
 #include <vector>
 
-struct RollResult
+namespace godot
 {
-	RollResult(const int sides, const std::vector<int> values)
+	enum RollResultFields
 	{
-		std::vector<int> sorted(values.size());
-		std::copy(values.begin(), values.end(), sorted.begin());
-		std::sort(sorted.begin(), sorted.end(), std::less<int>());
-		
-		auto total = 0;
-		for(auto val : sorted)
-			total += val;
-		
-		this->average = std::round(total / sorted.size());
-		this->highest = sorted.back();
-		this->lowest = sorted.front();
-		this->sides = sides;
-		this->total = total;
-		this->values = values;
-	}
+		Highest,
+		Lowest,
+		Sides,
+		Total,
+		Values,
+	};
 	
-	int average;
-	int highest;
-	int lowest;
-	int sides;
-	int total;
-	std::vector<int> values;
-};
+	struct RollResult
+	{
+		int highest;
+		int lowest;
+		int sides;
+		int total;
+		std::vector<int> values;
+		
+		RollResult(const int sides, const std::vector<int> values)
+		{
+			std::vector<int> sorted(values.size());
+			std::copy(values.begin(), values.end(), sorted.begin());
+			std::sort(sorted.begin(), sorted.end(), std::less<int>());
+			
+			auto total = 0;
+			for(auto val : sorted)
+				total += val;
+			
+			this->highest = sorted.back();
+			this->lowest = sorted.front();
+			this->sides = sides;
+			this->total = total;
+			this->values = values;
+		}
+		
+		RollResult(const Dictionary dict)
+		{
+			if(dict.has(RollResultFields::Highest))
+				highest = dict[RollResultFields::Highest];
+			if(dict.has(RollResultFields::Lowest))
+				lowest = dict[RollResultFields::Lowest];
+			if(dict.has(RollResultFields::Sides))
+				sides = dict[RollResultFields::Sides];
+			if(dict.has(RollResultFields::Total))
+				total = dict[RollResultFields::Total];
+			
+			if(dict.has(RollResultFields::Values))
+			{
+				TypedArray<int> received = dict[RollResultFields::Values];
+				auto size = received.size();
+				for(int64_t j = 0; j < size; j++)
+				{
+					values.push_back(received[j]);
+				}
+			}
+		}
+		
+		Dictionary into() const
+		{
+			Dictionary dict;
+			dict[RollResultFields::Highest] = highest;
+			dict[RollResultFields::Lowest] = lowest;
+			dict[RollResultFields::Sides] = sides;
+			dict[RollResultFields::Total] = total;
+			
+			TypedArray<int> valueArray;
+			for(auto val : values)
+				valueArray.append(val);
+			
+			dict[RollResultFields::Values] = valueArray;
+			
+			return dict;
+		}
+	};
+}
 
 #endif // ROLLRESULT_HH
